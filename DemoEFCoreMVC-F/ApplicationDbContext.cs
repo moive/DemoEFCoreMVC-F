ab9a9705aae791a00e7c638ec362b91dc0cb27eb
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-05-01 13:49:25 -0500

    config connection database sql before migration

---
+++ DemoEFCoreMVC-F/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DemoEFCoreMVC_F.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoEFCoreMVC_F
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ DemoEFCoreMVC-F/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoEFCoreMVC_F.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
