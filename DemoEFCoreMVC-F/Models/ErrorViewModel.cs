0edf7524c249959dd3d2a21ca5d593c3660bfe46
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-05-01 13:29:12 -0500

    init project

---
+++ DemoEFCoreMVC-F/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoEFCoreMVC_F.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
